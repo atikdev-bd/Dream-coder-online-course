1f45490ff35fac4db9c1e81a816b44bdb2dd6665
atikdev-bd <atikdev.bd@gmail.com>
2022-10-24 21:40:04 +0600

    seponsive navbar set

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header/Header";
+import RightSideNav from "../components/RightSideNav/RightSideNav";
+import "./Main.css";
 
 const Main = () => {
-    return (
+  return (
+    <>
+      <Header></Header>
+
+      <div className="row-container">
         <div>
-            
+          <Outlet></Outlet>
         </div>
-    );
+        <div>
+            <RightSideNav></RightSideNav>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Main;
