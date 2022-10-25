b629e2240b362c151f726d170a405d8f99497a5c
atikdev-bd <atikdev.bd@gmail.com>
2022-10-25 16:07:49 +0600

    logout icon set

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,69 @@
-import React, { useContext, useTransition } from "react";
+import React, { useContext } from "react";
+import { FaSignOutAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import ProfileIcon from "../../Assets/icon/profileIcon.png";
 import { AuthContext } from "../../context/Context";
-import ProfileIcon from '../../Assets/icon/profileIcon.png'
 
 const Header = () => {
-  const {user} = useContext(AuthContext)
-  console.log(user)
+  const { user } = useContext(AuthContext);
+  console.log(user);
   return (
     <div className="navbar bg-base-300 px-8">
       <div className="flex-1">
-        <Link className="btn  btn-ghost normal-case text-xl">DREAM COURSES</Link>
+        <Link className="btn  btn-ghost normal-case text-xl">
+          DREAM COURSES
+        </Link>
         <p>{user?.email}</p>
       </div>
       <div className="flex-none">
-      <div className="mr-8">
-        
-        <Link className="mr-4 font-bold" to='home'>Home</Link>
-        <Link className="mr-4 font-bold" to='blog'>Blog</Link>
-        <Link className="mr-4 font-bold" to='about'>About</Link>
-        <Link className="mr-4 font-bold" to='register'>Register</Link>
-        <Link className="mr-4 font-bold" to='login'>Login</Link>
-      </div>
+        <div className="mr-8">
+          {user && user?.uid ? (
+            <div className="flex items-center">
+              <Link className="mr-4 font-bold" to="home">
+                Home
+              </Link>
+              <Link className="mr-4 font-bold" to="blog">
+                Blog
+              </Link>
+              <Link className="mr-4 font-bold" to="about">
+                About
+              </Link>
+             <Link to='/home'>
+                {" "}
+                <FaSignOutAlt className="text-gray-900 ml-2 hover:text-red-500"></FaSignOutAlt>{" "}
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link className="mr-4 font-bold" to="home">
+                Home
+              </Link>
+              <Link className="mr-4 font-bold" to="blog">
+                Blog
+              </Link>
+              <Link className="mr-4 font-bold" to="about">
+                About
+              </Link>
+              <Link className="mr-4 font-bold" to="register">
+                Register
+              </Link>
+              <Link className="mr-4 font-bold" to="login">
+                Login
+              </Link>
+            </>
+          )}
+        </div>
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div title={user.displayName ? user.displayName : 'No name found'} className="w-10 rounded-full">
-
-              {
-                user && user?.photoURL ? <img src={user.photoURL}alt=""/> : <img src={ProfileIcon}alt=""/>
-              }
+            <div
+              title={user.displayName ? user.displayName : "No name found"}
+              className="w-10 rounded-full"
+            >
+              {user && user?.photoURL ? (
+                <img src={user.photoURL} alt="" />
+              ) : (
+                <img src={ProfileIcon} alt="" />
+              )}
             </div>
           </label>
           <ul
