2b135ca365d13f6f9c1874d5be61ffec52b5edf7
atikdev-bd <atikdev.bd@gmail.com>
2022-10-25 13:40:17 +0600

    footer added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5HVhh2JPPXULRUa2t6-SFIyVOr7A8wKo",
+  authDomain: "dream-coder-auth.firebaseapp.com",
+  projectId: "dream-coder-auth",
+  storageBucket: "dream-coder-auth.appspot.com",
+  messagingSenderId: "473383297249",
+  appId: "1:473383297249:web:199b11975260256276d7a0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
